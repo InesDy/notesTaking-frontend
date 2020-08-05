df0c69f6cf4ede447f3baf5c3e248230b0b1d8a8
Ines Dyll <inesdyll1@gmail.com>
2020-08-05 19:13:46 +0200

    added icons

--- src/components/FolderItem/FolderItem.js
+++ src/components/FolderItem/FolderItem.js
@@ -1,34 +1,33 @@
-import React from 'react';
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const FolderItem = ({
-    folder,
-    selectedFolder,
-    updateSelectedFolder,
-    deleteFolder,
+  folder,
+  selectedFolder,
+  updateSelectedFolder,
+  deleteFolder,
 }) => (
-        <div
-            className="folder"
-            key={folder.id}
-            disabled={selectedFolder && selectedFolder.id === folder.id}
-            onClick={() => updateSelectedFolder(folder)}
-        >
-            <div className="folder__name">
-                {folder.name}
-            </div>
+  <div
+    className="folder"
+    key={folder.id}
+    disabled={selectedFolder && selectedFolder.id === folder.id}
+    onClick={() => updateSelectedFolder(folder)}
+  >
+    <div className="folder__name">{folder.name}</div>
 
-            {folder.name.toLowerCase() !== 'trash' && (
-                <div className="folder__actions">
-                    <button
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            deleteFolder(folder.id);
-                        }}
-                    >
-                        Delete
-                </button>
-                </div>
-            )}
-        </div>
-    )
+    {folder.name.toLowerCase() !== "trash" && (
+      <div className="folder__actions">
+        <DeleteIcon
+          onClick={(event) => {
+            event.stopPropagation();
+            deleteFolder(folder.id);
+          }}
+        >
+          Delete
+        </DeleteIcon>
+      </div>
+    )}
+  </div>
+);
 
 export default FolderItem;
--- src/components/NoteItem/NoteItem.js
+++ src/components/NoteItem/NoteItem.js
@@ -1,37 +1,36 @@
-import React from 'react';
+import React from "react";
+import DeleteSweepIcon from "@material-ui/icons/DeleteSweep";
 
 const NoteItem = ({
-    note,
-    updateSelectedNote,
-    selectedNote,
-    onDeleteButtonClick,
+  note,
+  updateSelectedNote,
+  selectedNote,
+  onDeleteButtonClick,
 }) => (
-        <div
-            className="Notes-item"
-            key={note.id}
-            onClick={() => updateSelectedNote(note)}
-        >
-            {!selectedNote && note.text.slice(0, 30) + "..."}
+  <div
+    className="Notes-item"
+    key={note.id}
+    onClick={() => updateSelectedNote(note)}
+  >
+    {!selectedNote && note.text.slice(0, 30) + "..."}
 
-            {selectedNote &&
-                selectedNote.id !== note.id &&
-                note.text.slice(0, 50) + "..."
-            }
+    {selectedNote &&
+      selectedNote.id !== note.id &&
+      note.text.slice(0, 50) + "..."}
 
-            {selectedNote &&
-                selectedNote.id === note.id &&
-                selectedNote.text.slice(0, 50) + "..."
-            }
+    {selectedNote &&
+      selectedNote.id === note.id &&
+      selectedNote.text.slice(0, 50) + "..."}
 
-            <button
-                onClick={(event) => {
-                    event.stopPropagation();
-                    onDeleteButtonClick(note.id);
-                }}
-            >
-                Delete
-            </button>
-        </div>
-    );
+    <DeleteSweepIcon
+      onClick={(event) => {
+        event.stopPropagation();
+        onDeleteButtonClick(note.id);
+      }}
+    >
+      Delete
+    </DeleteSweepIcon>
+  </div>
+);
 
 export default NoteItem;
